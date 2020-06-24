547871c90c3b33c947a12221106571a9e2788960
DouglasFerreira92 <douglasferreira9292@gmail.com>
2020-06-24 17:51:58 -0300

    Atualizei phpmailer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit05ef1faff23b48b1bf965634b58a5ee4
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+    );
+
     public static $fallbackDirsPsr4 = array (
         0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
     );
@@ -27,23 +41,13 @@ class ComposerStaticInit05ef1faff23b48b1bf965634b58a5ee4
         ),
     );
 
-    public static $classMap = array (
-        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
-        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
-        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
-        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
-        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
-        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
-        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
-        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit05ef1faff23b48b1bf965634b58a5ee4::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit05ef1faff23b48b1bf965634b58a5ee4::$prefixDirsPsr4;
             $loader->fallbackDirsPsr4 = ComposerStaticInit05ef1faff23b48b1bf965634b58a5ee4::$fallbackDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit05ef1faff23b48b1bf965634b58a5ee4::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit05ef1faff23b48b1bf965634b58a5ee4::$classMap;
 
         }, null, ClassLoader::class);
     }
